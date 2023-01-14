c757f10f1afe85788c6b9b954b5128c11d6756a2
Westyn Drake <70765092+WestynD@users.noreply.github.com>
2023-01-14 11:41:18 -0700

    Trying to get this bugger to work

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-#git push -f git@github.com:WestynD/WestynD.github.io.git main:gh-pages
+git push -f git@github.com:WestynD/WestynD.github.io.git main:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
